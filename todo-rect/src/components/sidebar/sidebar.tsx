1f54b4ad861a73a95dd9a32010f85637c40dbd2f
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-13 08:43:27 +0100

    Profile Component Layout

--- todo-rect/src/components/sidebar/sidebar.tsx
+++ todo-rect/src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from 'react'; 
 import { Grid } from "@mui/material";
+import { Profile } from '../profile/profile';
 
 export const Sidebar: FC = (): ReactElement => {
     return(  <Grid
@@ -18,6 +19,6 @@ export const Sidebar: FC = (): ReactElement => {
           alignItems: 'center',
         }}
       >
-        <h2>Sidebar Area</h2>
+        <Profile />
       </Grid>)
 }
