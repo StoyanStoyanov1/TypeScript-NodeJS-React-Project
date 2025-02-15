d9a762226a823b2b5a9e6cc97d42281ea590431d
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-15 17:35:29 +0100

    Making components dynamic using interfaces

--- todo-rect/src/components/CreateTaskForm/_taskDescriptionField.tsx
+++ todo-rect/src/components/CreateTaskForm/_taskDescriptionField.tsx
@@ -1,7 +1,13 @@
 import React, {FC, ReactElement} from "react";
 import { TextField } from "@mui/material";
+import { ITextField } from "./interfaces/ITextField";
 
-export const TaskDescriptionField: FC = (): ReactElement => {
+export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
+    const { 
+        onChange = (e) => console.log(e), 
+        disabled = false, 
+    } = props;
+    
     return (
         <TextField
         id="description"
@@ -13,6 +19,8 @@ export const TaskDescriptionField: FC = (): ReactElement => {
         multiline
         fullWidth
         rows={4}
+        disabled={disabled}
+        onChange={onChange}
         />
     )
 
