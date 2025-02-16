82d97cc3abbe5cf4f01473224bf363594084e2ba
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-16 09:29:37 +0100

    Add Props to DateField

--- todo-rect/src/components/CreateTaskForm/_taskDateField.tsx
+++ todo-rect/src/components/CreateTaskForm/_taskDateField.tsx
@@ -1,21 +1,40 @@
-import React, { FC, ReactElement, useState } from "react";
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import React, { FC, ReactElement } from 'react';
+
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import dayjs, { Dayjs } from 'dayjs';
+import { IDateField } from './interfaces/IDateField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import PropTypes from 'prop-types';
+import { TextField } from '@mui/material';
+import dayjs from 'dayjs'; // Import dayjs for date conversion
 
-export const TaskDateField: FC = (): ReactElement => {
-    const [date, setDate] = useState<Dayjs | null>(dayjs());
+export const TaskDateField: FC<IDateField> = (
+  props,
+): ReactElement => {
+  // Destructure props
+  const {
+    value = new Date(),
+    disabled = false,
+    onChange = (date) => console.log(date),
+  } = props;
 
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
-                label="Task Date"
-                value={date}
-                onChange={(newValue) => setDate(newValue)}
-                views={['year', 'month', 'day']}
-                slotProps={{ textField: { fullWidth: true } }}
-            />
-        </LocalizationProvider>
-    );
-}
+  const dayjsValue = dayjs(value);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DesktopDatePicker
+        label="Task Date"
+        value={dayjsValue.isValid() ? dayjsValue : null} 
+        onChange={onChange}
+        views={['year', 'month', 'day']}
+        slotProps={{ textField: { fullWidth: true } }}
+        disabled={disabled}
+      />
+    </LocalizationProvider>
+  );
+};
+
+TaskDateField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+};
---
+++ todo-rect/src/components/CreateTaskForm/interfaces/IDateField.ts
@@ -0,0 +1,6 @@
+import { IDisabled } from './IDisabled';
+import { Dayjs } from 'dayjs'; 
+export interface IDateField extends IDisabled {
+  value?: Dayjs | null;  
+  onChange?: (date: Dayjs | null) => void;  
+}
