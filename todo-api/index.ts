805468a8d56fc588a70cf94fc27b0987aef96efd
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-17 10:33:46 +0100

    Adding other dependencies

--- todo-api/index.ts
+++ todo-api/index.ts
@@ -1,10 +1,31 @@
 import express, {Express, Request, Response} from "express";
 
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import cors from 'cors';
+import bodyParser from "body-parser";
 
 // Instantiate express app
 const app: Express = express();
 dotenv.config();
+
+// Parse request Body
+app.use(bodyParser.json());
+
+// Use CORS install types as well
+
+app.use(cors());
+
+// Create Database Connection
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    synchronize: true,
+});
 
 // Define sever port
 const port = process.env.PORT;
@@ -14,5 +35,11 @@ app.get('/', (req: Request, res: Response)=>{
     res.send('Exress + TypeScript Server')
 });
 
+AppDataSource.initialize().then(() => {
+    app.listen(port);
+    console.log('Data Source has been initialized');
+}).catch((err) => {
+    console.error('Error during Data Source initialization')
+});
+
 // Start listenting to the requests on the defined port
-app.listen(port)
