7d19ff9583175a9205d6cbeaf453dd22ac9a3aa7
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-18 11:43:38 +0100

    Refactoring Router and Controller

--- todo-api/src/tasks/tasks.router.ts
+++ todo-api/src/tasks/tasks.router.ts
@@ -1,21 +1,14 @@
 import { Request, Response, Router } from 'express';
 
-import { TasksController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
+import { taskController } from './tasks.controller';
 import { validationResult } from 'express-validator';
 
 /*Fire the router function*/
 export const tasksRouter: Router = Router();
 
 // Create a default route.
-tasksRouter.get(
-  '/tasks',
-  async (req: Request, res: Response) => {
-    const taskController = new TasksController();
-    const allTasks = await taskController.getAll();
-    res.json(allTasks).status(200);
-  },
-);
+tasksRouter.get('/tasks', taskController.getAll);
 
 tasksRouter.post(
   '/tasks',
