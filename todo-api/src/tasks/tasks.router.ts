e92226f0c33f9b7119ab0eb6280b891b5d914c86
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-02-18 11:35:56 +0100

    Using the validator

--- todo-api/src/tasks/tasks.router.ts
+++ todo-api/src/tasks/tasks.router.ts
@@ -1,18 +1,33 @@
-import { Request, Response, Router } from 'express'; 
+import { Request, Response, Router } from 'express';
+
 import { TasksController } from './tasks.controller';
+import { createValidator } from './tasks.validator';
+import { validationResult } from 'express-validator';
 
 /*Fire the router function*/
 export const tasksRouter: Router = Router();
 
-tasksRouter.get('/tasks', 
-    async (req: Request, res: Response) => {
-        const TaskController = new TasksController();
-        const allTasks = await TaskController.getAll();
-        res.json(allTasks).status(200);
-});
+// Create a default route.
+tasksRouter.get(
+  '/tasks',
+  async (req: Request, res: Response) => {
+    const taskController = new TasksController();
+    const allTasks = await taskController.getAll();
+    res.json(allTasks).status(200);
+  },
+);
 
 tasksRouter.post(
-    '/tasks', 
-    (req: Request, res: Response) => {
-         
-});
+  '/tasks',
+  createValidator,
+  //@ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+  },
+);
